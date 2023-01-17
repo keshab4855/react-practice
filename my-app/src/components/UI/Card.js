3cf3f966dff90e1a1169c8af43b5e8c41e447510
keshab4855 <keshabroka123@gmail.com>
2023-01-17 14:04:47 +1100

    getting deep into react from basics

--- my-app/src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Card.css";
-
-const Card = (expenses) => {
-  const classes = "card " + expenses.className;
-  return <div className={classes}>{expenses.children}</div>;
-};
-
-export default Card;
