511d853c1e595033858ac5c17933df3ab3b9318f
keshab4855 <keshabroka123@gmail.com>
2023-01-18 13:51:12 +1100

    adding bulma css-library

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,41 @@
 import "./App.css";
+import "bulma/css/bulma.css";
 import ProfileCard from "./components/ProfileCard";
+import ALexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistance</div>
-      <ProfileCard title={"Alexa"} handle={"@alexa99"}></ProfileCard>
-      <ProfileCard title={"Cortana"} handle={"@cortana88"}></ProfileCard>
-      <ProfileCard title={"Siri"} handle={"@siri123"}></ProfileCard>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard
+                title={"Alexa"}
+                handle={"@alexa99"}
+                image={ALexaImage}
+              ></ProfileCard>
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title={"Cortana"}
+                handle={"@cortana88"}
+                image={CortanaImage}
+              ></ProfileCard>
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title={"Siri"}
+                handle={"@siri123"}
+                image={SiriImage}
+              ></ProfileCard>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- my-app/src/components/ProfileCard.js
+++ my-app/src/components/ProfileCard.js
@@ -1,10 +1,19 @@
 import React from "react";
 
-const ProfileCard = ({ title, handle }) => {
+const ProfileCard = ({ title, handle, image }) => {
   return (
-    <div>
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
+    <div className="card ">
+      <div className="card-image">
+        <figure class="image is-4by3">
+          <img className="card-image" src={image} alt="" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+      </div>
     </div>
   );
 };
