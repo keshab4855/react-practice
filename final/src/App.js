ba1d8010c1426988db1f98d602fafa24e8c35f36
keshab4855 <keshabroka123@gmail.com>
2023-01-21 18:12:09 +1100

    starting using API with react

--- final/src/App.js
+++ final/src/App.js
@@ -1,27 +1,7 @@
-import { useState } from "react";
-import AnimalShow from "../../final/src/AnimalShow";
 import "./App.css";
-function getRandomAnimals() {
-  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
-  return animals[Math.floor(Math.random() * animals.length)];
-}
 
 function App() {
-  const [animals, setAnimals] = useState([]);
-
-  const handleOnClick = () => {
-    setAnimals([...animals, getRandomAnimals()]);
-  };
-  const renderedAnimals = animals.map((item, index) => {
-    console.log(animals);
-    return <AnimalShow type={item} key={index}></AnimalShow>;
-  });
-  return (
-    <div className="app">
-      <button onClick={handleOnClick}>Add Animal</button>
-      <div className="animal-list">{renderedAnimals}</div>
-    </div>
-  );
+  return <div className="app">Hello</div>;
 }
 
 export default App;
