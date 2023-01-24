46a6e4ac0e53ea7d5a884dfcfd51b1e9875b5c43
keshab4855 <keshabroka123@gmail.com>
2023-01-24 13:14:26 +1100

    starting Form handling

--- final/src/App.js
+++ final/src/App.js
@@ -1,21 +1,7 @@
-import searchImages from "./api";
 import "./App.css";
-import SearchBar from "./components/SearchBar";
-import ImageList from "./components/ImageList";
-import { useState } from "react";
 
 function App() {
-  const [images, setImages] = useState([]);
-  const handleOnSubmit = async (term) => {
-    const result = await searchImages(term);
-    setImages(result);
-  };
-  return (
-    <div className="app">
-      <SearchBar handleOnSubmit={handleOnSubmit}></SearchBar>
-      <ImageList images={images}></ImageList>
-    </div>
-  );
+  return <div className="app">hello</div>;
 }
 
 export default App;
