01341a67e6706223d209546abcd9dff1cc011810
keshab4855 <keshabroka123@gmail.com>
2023-01-26 14:07:08 +1100

    receiveing new title

--- final/src/App.js
+++ final/src/App.js
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import "./App.css";
+import BookCreate from "./components/BookCreate";
 
 function App() {
   const [books, setBooks] = useState([]);
+  const createBook = (title) => {
+    console.log("Need to add book with :", title);
+  };
 
-  const createBook
-  return <div className="app">hello</div>;
+  return (
+    <div className="app">
+      <BookCreate createBook={createBook}></BookCreate>
+    </div>
+  );
 }
 
 export default App;
--- final/src/components/BookCreate.js
+++ final/src/components/BookCreate.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-const BookCreate = () => {
-  return <div>BookCreate</div>;
+const BookCreate = ({ createBook }) => {
+  const [title, setTitle] = useState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    createBook(title);
+    setTitle("");
+  };
+  const changeHandler = (e) => {
+    const typedTitle = e.target.value;
+    setTitle(typedTitle);
+  };
+
+  return (
+    <div>
+      <form action="" type="submit" onSubmit={submitHandler}>
+        <label htmlFor="">Title</label>
+        <input onChange={changeHandler} value={title} type="text" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default BookCreate;
