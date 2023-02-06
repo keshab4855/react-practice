6a4d79cee3db7ee895de0e98e95a70db05c84036
keshab4855 <keshabroka123@gmail.com>
2023-02-07 04:31:20 +1100

    children props

--- final/src/App.js
+++ final/src/App.js
@@ -1,5 +1,25 @@
+import Button from "./Button";
+
 function App() {
-  return <div className="app">hello</div>;
+  return (
+    <div className="app">
+      <div>
+        <Button>Click Here</Button>
+      </div>
+      <div>
+        <Button>Buy Now!</Button>
+      </div>
+      <div>
+        <Button>See Deal!</Button>
+      </div>
+      <div>
+        <Button>Hide Adds </Button>
+      </div>
+      <div>
+        <Button>Something</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
