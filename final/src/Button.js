055ccd608d5cd56b949e32648c6d2628f79a7c43
keshab4855 <keshabroka123@gmail.com>
2023-02-08 02:49:52 +1100

    propTypes in action

--- final/src/Button.js
+++ final/src/Button.js
@@ -1,7 +1,33 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Button = ({ children }) => {
+const Button = ({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+}) => {
   return <button>{children}</button>;
+};
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!warning) +
+      Number(!!success) +
+      Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        "Only one of primary, secondary, success, warning or danger is true"
+      );
+    }
+  },
 };
 
 export default Button;
