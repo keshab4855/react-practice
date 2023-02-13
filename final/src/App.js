e8cc23dfbf911c4169d33ec6764124dee57d48b5
keshab4855 <keshabroka123@gmail.com>
2023-02-14 02:47:18 +1100

    adding color

--- final/src/App.js
+++ final/src/App.js
@@ -3,10 +3,10 @@ import Table from "./components/Table";
 function App() {
   const data = [
     { name: "Orange", color: "bg-orange-500", score: 5 },
-    { name: "Apple", color: "bg-orange-500", score: 5 },
-    { name: "Grapes", color: "bg-orange-500", score: 5 },
-    { name: "lime", color: "bg-orange-500", score: 5 },
-    { name: "Banana", color: "bg-orange-500", score: 5 },
+    { name: "Apple", color: "bg-red-500", score: 5 },
+    { name: "Grapes", color: "bg-green-200", score: 5 },
+    { name: "lime", color: "bg-green-600", score: 5 },
+    { name: "Banana", color: "bg-yellow-500", score: 5 },
   ];
   return (
     <div>
