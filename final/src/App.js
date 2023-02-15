0dd6df7727dc1866a51dc37eafd3687dc15bdb07
keshab4855 <keshabroka123@gmail.com>
2023-02-15 14:14:53 +1100

    cleaning the app for redux

--- final/src/App.js
+++ final/src/App.js
@@ -1,18 +1,5 @@
-import Table from "./components/Table";
-
 function App() {
-  const data = [
-    { name: "Orange", color: "bg-orange-500", score: 5 },
-    { name: "Apple", color: "bg-red-500", score: 5 },
-    { name: "Grapes", color: "bg-green-200", score: 5 },
-    { name: "lime", color: "bg-green-600", score: 5 },
-    { name: "Banana", color: "bg-yellow-500", score: 5 },
-  ];
-  return (
-    <div>
-      <Table data={data}></Table>
-    </div>
-  );
+  return <div>hello</div>;
 }
 
 export default App;
