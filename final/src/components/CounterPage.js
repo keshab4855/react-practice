6ab100c89847eff3903c82db244567be5f11f6ca
keshab4855 <keshabroka123@gmail.com>
2023-02-17 03:52:15 +1100

    adding the form

--- final/src/components/CounterPage.js
+++ final/src/components/CounterPage.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { useState } from "react";
 
 const CounterPage = ({ count, handleOnSubmit }) => {
+  const [valueToAdd, setValueToAdd] = useState(0);
   const increment = () => {
     const updatedCount = count + 1;
     return handleOnSubmit(updatedCount);
@@ -9,11 +11,31 @@ const CounterPage = ({ count, handleOnSubmit }) => {
     const updatedCount = count - 1;
     return handleOnSubmit(updatedCount);
   };
+
+  const handleChange = (e) => {
+    const finalValue = parseInt(e.target.value);
+
+    setValueToAdd(finalValue);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleOnSubmit(count + valueToAdd);
+    setValueToAdd(0);
+  };
   return (
     <div>
-      <h1>Count is {count}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <div>
+        <h1>Count is {count}</h1>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">Add a lot!</label>
+        <input value={valueToAdd || ""} onChange={handleChange} type="number" />
+        <button>Add it</button>
+      </form>
     </div>
   );
 };
