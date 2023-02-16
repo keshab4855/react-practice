6ba94337416cfbb32d4c5e7288f96b92899659f3
keshab4855 <keshabroka123@gmail.com>
2023-02-16 13:21:52 +1100

    applying increment and decrement

--- final/src/components/CounterPage.js
+++ final/src/components/CounterPage.js
@@ -5,10 +5,15 @@ const CounterPage = ({ count, handleOnSubmit }) => {
     const updatedCount = count + 1;
     return handleOnSubmit(updatedCount);
   };
+  const decrement = () => {
+    const updatedCount = count - 1;
+    return handleOnSubmit(updatedCount);
+  };
   return (
     <div>
       <h1>Count is {count}</h1>
       <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 };
